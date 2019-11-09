9c7a576e56041ffb29b0e297fd30b5f6dd6bcdfe
John Azzaro <jazzaro@temple.edu>
2019-11-09 15:59:11 -0800

    create basic server

--- server.js
+++ server.js
@@ -1,14 +1,3 @@
-// const express = require('express');
-// const app = express();
+const express = required('express');
+const app = express();
 
-
-
-// app.get('/', function(req, res) {
-//     res.send('the server works!');
-// })
-
-// let server;
-
-// server = app.listen(3000, function() {
-//     console.log('Your app is listening on port 3000...')
-// });
