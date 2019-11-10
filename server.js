3f81f656baffc49667a558f46c5606cd32cc089e
John Azzaro <jazzaro@temple.edu>
2019-11-09 17:26:23 -0800

    added runServer

--- server.js
+++ server.js
@@ -19,10 +19,36 @@ server = app.listen('3000', function() {
 })
 
 
-function runServer() {
+function runServer(databaseUrl, port=PORT) { 
+    return new Promise( function(resolve, reject) {    
+        mongoose
+            .connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) { 
+              if (err) {          
+                  return reject(err);  
+              }       
 
-}
+              server = app.listen(port, function() {    
+                  console.log(`Listening on port ${port}...`);  
+                  resolve();   
+              })
+              .on('error', function(err) {  
+                  mongoose.disconnect();  
+                  reject(err);  
+              });
+        });
+    });
+}  
 
-function closeServer() {
-    
-}
+function closeServer() {                                                                 // 3.8 - Close server (for integration tests).
+    return mongoose.disconnect().then(() => {
+      return new Promise((resolve, reject) => {
+        console.log('Closing server');
+        server.close(err => {
+          if (err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
+  }
