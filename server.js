eb4e36904c6f392f4f35d46aa108f2540b48d183
John Azzaro <jazzaro@temple.edu>
2019-11-09 16:05:51 -0800

    updated server test

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
-const express = required('express');
+const express = require('express');
 const app = express();
 
+
+app.get('/', function(req, res) {
+    res.send('This is a test')
+})
+
+let server;
+server = app.listen('3000', function() {
+    console.log('Your app is listening on port 3000...');
+})
