5ef946fed930fd66bef56ab31d54c87814500931
John Azzaro <jazzaro@temple.edu>
2019-11-09 17:30:27 -0800

    added server functionality

--- server.js
+++ server.js
@@ -14,41 +14,41 @@ app.get('/', function(req, res) {
 })
 
 let server;
-server = app.listen('3000', function() {
-    console.log('Your app is listening on port 3000...');
-})
 
-
-function runServer(databaseUrl, port=PORT) { 
-    return new Promise( function(resolve, reject) {    
-        mongoose
-            .connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) { 
-              if (err) {          
-                  return reject(err);  
-              }       
-
-              server = app.listen(port, function() {    
-                  console.log(`Listening on port ${port}...`);  
-                  resolve();   
-              })
-              .on('error', function(err) {  
-                  mongoose.disconnect();  
-                  reject(err);  
-              });
-        });
-    });
-}  
-
-function closeServer() {                                                                 // 3.8 - Close server (for integration tests).
-    return mongoose.disconnect().then(() => {
-      return new Promise((resolve, reject) => {
-        console.log('Closing server');
-        server.close(err => {
-          if (err) {
+function runServer(databaseUrl, port = PORT) {
+   return new Promise(function(resolve, reject) {
+      mongoose.connect(databaseUrl, { useNewUrlParser: true, useUnifiedTopology: true }, function(err) {
+         if (err) {
             return reject(err);
-          }
-          resolve();
-        });
+         }
+
+         server = app
+            .listen(port, function() {
+               console.log(`Listening on port ${port}...`);
+               resolve();
+            })
+            .on('error', function(err) {
+               mongoose.disconnect();
+               reject(err);
+            });
       });
-    });
-  }
+   });
+}
+
+function closeServer() {
+   return mongoose.disconnect().then(function() {
+      return new Promise(function(resolve, reject) {
+         console.log('Closing server');
+         server.close(function(err) {
+            if (err) {
+               return reject(err);
+            }
+            resolve();
+         });
+      });
+   });
+}
+
+if (require.main === module) { 
+    runServer(DATABASE_URL).catch(err => console.error(err));
+}
