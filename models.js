be9cbb2517397912f43a5749d0e601db8e9de5a0
John Azzaro <jazzaro@temple.edu>
2019-11-11 14:12:20 -0800

    update middleware, mongoose

--- models.js
+++ models.js
@@ -2,3 +2,14 @@
 
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+
+let authorSchema = mongoose.Schema({
+    firstName: 'string',
+    lastName: 'string',
+    userName: {
+      type: 'string',
+      unique: true
+    }
+});
+
+var Author = mongoose.model('Author', authorSchema);
