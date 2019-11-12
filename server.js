bc731074fe4973f8d6680d80b33ebcdb960733e4
John Azzaro <jazzaro@temple.edu>
2019-11-11 17:43:45 -0800

    POST route for authors

--- server.js
+++ server.js
@@ -42,7 +42,48 @@ app.get('/authors', (req, res) => {
  });
 
 
-// 
+// POST route for authors
+app.post('/authors', (req, res) => {
+   const requiredFields = ['firstName', 'lastName', 'userName'];
+   requiredFields.forEach(field => {
+     if (!(field in req.body)) {
+       const message = `Missing \`${field}\` in request body`;
+       console.error(message);
+       return res.status(400).send(message);
+     }
+   });
+ 
+   Author
+     .findOne({ userName: req.body.userName })
+     .then(author => {
+       if (author) {
+         const message = `Username already taken`;
+         console.error(message);
+         return res.status(400).send(message);
+       }
+       else {
+         Author
+           .create({
+             firstName: req.body.firstName,
+             lastName: req.body.lastName,
+             userName: req.body.userName
+           })
+           .then(author => res.status(201).json({
+               _id: author.id,
+               name: `${author.firstName} ${author.lastName}`,
+               userName: author.userName
+             }))
+           .catch(err => {
+             console.error(err);
+             res.status(500).json({ error: 'Something went wrong' });
+           });
+       }
+     })
+     .catch(err => {
+       console.error(err);
+       res.status(500).json({ error: 'something went horribly awry' });
+     });
+ });
 
 
 
